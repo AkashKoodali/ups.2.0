6ca7de12af695a536674c4f81630d0e464f537e0
Akash Koodali <akashkoodali9@gmail.com>
2023-07-30 15:30:23 +0530

    modal-screen and react-map added

--- screens/ModalScreen.tsx
+++ screens/ModalScreen.tsx
@@ -1,10 +1,63 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
+import { Icon } from "@rneui/themed";
+import { useTailwind } from "tailwind-rn";
+import {
+  CompositeNavigationProp,
+  RouteProp,
+  useNavigation,
+  useRoute,
+} from "@react-navigation/native";
+import { TabStackParamList } from "../navigator/TabNavigator";
+import { RootStackParamList } from "../navigator/RootNavigator";
+import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import useCustomerOrders from "../hooks/useCustomerOrders.tsx";
+import { FlatList } from "react-native";
+import DeliveryCard from "../components/DeliveryCard";
+
+type ModalScreenNavigationProp = CompositeNavigationProp<
+  BottomTabNavigationProp<TabStackParamList>,
+  NativeStackNavigationProp<RootStackParamList, "MyModal">
+>;
+
+type ModalScreenRootProp = RouteProp<RootStackParamList, "MyModal">;
 
 const ModalScreen = () => {
+  const tw = useTailwind();
+  const navigation = useNavigation<ModalScreenNavigationProp>();
+  const {
+    params: { name, userId },
+  } = useRoute<ModalScreenRootProp>();
+
+  const { loading, error, orders } = useCustomerOrders(userId);
+
   return (
     <View>
-      <Text>ModalScreen</Text>
+      <TouchableOpacity
+        onPress={navigation.goBack}
+        style={tw("absolute right-5 top-5 z-10")}
+      >
+        <Icon name="closecircle" type="antdesign" />
+      </TouchableOpacity>
+
+      <View style={{ marginTop: 10 }}>
+        <View style={[tw("py-5 border-b"), { borderColor: "#59C1CC" }]}>
+          <Text
+            style={[tw("text-center text-xl font-bold"), { color: "#59C1CC" }]}
+          >
+            {name}
+          </Text>
+          <Text style={[tw("text-center italic text-sm")]}>deliveries</Text>
+        </View>
+
+        <FlatList
+          contentContainerStyle={{ paddingBottom: 200 }}
+          data={orders}
+          keyExtractor={(orders) => orders.trackingId}
+          renderItem={({ item: order }) => <DeliveryCard order={order} />}
+        />
+      </View>
     </View>
   );
 };
